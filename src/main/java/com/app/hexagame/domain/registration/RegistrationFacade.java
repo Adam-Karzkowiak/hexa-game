2bc5beeb962effcef63a22d1bfccef6fd7d03421
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 23:56:56 +0100

    isPasswordValid method created

--- src/main/java/com/app/hexagame/domain/registration/RegistrationFacade.java
+++ src/main/java/com/app/hexagame/domain/registration/RegistrationFacade.java
@@ -1,11 +1,24 @@
 package com.app.hexagame.domain.registration;
 
 
+import com.app.hexagame.domain.registration.dto.RegistrationWriteModel;
+import com.app.hexagame.domain.utility.IdentifierProvider;
+import com.app.hexagame.domain.utility.ValidationService;
+
 public class RegistrationFacade {
 
     private final RegistrationRepository repository;
+    private final ValidationService validation;
+    private final IdentifierProvider identifier;
 
-    public RegistrationFacade(final RegistrationRepository repository) {
+    public RegistrationFacade(final RegistrationRepository repository, final ValidationService validation, final IdentifierProvider identifier) {
         this.repository = repository;
+        this.validation = validation;
+        this.identifier = identifier;
+    }
+
+
+    Registration simpleRegistration(RegistrationWriteModel writeModel) {
+
     }
 }
--- src/main/java/com/app/hexagame/domain/utility/ValidationService.java
+++ src/main/java/com/app/hexagame/domain/utility/ValidationService.java
@@ -1,4 +1,24 @@
 package com.app.hexagame.domain.utility;
 
-class ValidationService {
+public class ValidationService {
+
+    public void isPasswordValid(String password) {
+
+        if (password.length() > 20 || password.length() < 8) {
+            throw new IllegalArgumentException("Password must be less than 20 and more than 8 characters in length.");
+        }
+        String upperCaseChars = "(.*[A-Z].*)";
+        if (!password.matches(upperCaseChars)) {
+            throw new IllegalArgumentException("Password must have at least one uppercase character.");
+        }
+        String lowerCaseChars = "(.*[a-z].*)";
+        if (!password.matches(lowerCaseChars)) {
+            throw new IllegalArgumentException("Password must have at least one lowercase character");
+        }
+        String numbers = "(.*[0-9].*)";
+        if (!password.matches(numbers)) {
+            throw new IllegalArgumentException("Password must have at least one number");
+        }
+
+    }
 }
