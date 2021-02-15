9228637188572742bf152a84eb0e383f5bd15850
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 22:13:12 +0100

    Initial commit

---
+++ src/main/java/com/app/hexagame/HexaGameApplication.java
@@ -0,0 +1,13 @@
+package com.app.hexagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/hexagame/HexaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hexagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
