f2f2fe0c47b1143389a13ddb1adc20edb5c8aa45
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 22:51:42 +0100

    JpaRepository

--- src/main/java/com/app/hexagame/domain/registration/RegistrationRepository.java
+++ src/main/java/com/app/hexagame/domain/registration/RegistrationRepository.java
@@ -1,8 +1,9 @@
 package com.app.hexagame.domain.registration;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface RegistrationRepository {
+interface RegistrationRepository extends JpaRepository<Integer, Registration> {
 
 }
