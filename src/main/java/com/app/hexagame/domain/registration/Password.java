ff687b5fe38ae9c6201c726e89b1fe2af2f64d94
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 23:32:33 +0100

    Password constructor refactored

--- src/main/java/com/app/hexagame/domain/registration/Password.java
+++ src/main/java/com/app/hexagame/domain/registration/Password.java
@@ -1,11 +1,9 @@
 package com.app.hexagame.domain.registration;
 
-import org.springframework.security.crypto.bcrypt.BCrypt;
-
 class Password {
     String password;
 
     Password(final String password) {
-        BCrypt.hashpw(password, this.password);
+        this.password = password;
     }
 }
