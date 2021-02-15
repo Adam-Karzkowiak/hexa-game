e8f1d744a7b8412d69164a3865b954f79b49f189
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 23:57:30 +0100

    Registration class fields created

--- src/main/java/com/app/hexagame/domain/registration/Registration.java
+++ src/main/java/com/app/hexagame/domain/registration/Registration.java
@@ -1,8 +1,12 @@
 package com.app.hexagame.domain.registration;
 
 
+import java.time.LocalDateTime;
+
 class Registration {
-
-
+    int id;
+    String email;
+    String password;
+    LocalDateTime localDateTime;
 
 }
