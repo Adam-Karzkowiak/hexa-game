ff52792eca868906d7b3aceff9069a8d289d012a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-15 23:58:13 +0100

    BCryptPasswordEncoder added to class fields

--- src/main/java/com/app/hexagame/domain/registration/RegistrationFacade.java
+++ src/main/java/com/app/hexagame/domain/registration/RegistrationFacade.java
@@ -4,17 +4,20 @@ package com.app.hexagame.domain.registration;
 import com.app.hexagame.domain.registration.dto.RegistrationWriteModel;
 import com.app.hexagame.domain.utility.IdentifierProvider;
 import com.app.hexagame.domain.utility.ValidationService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class RegistrationFacade {
 
     private final RegistrationRepository repository;
     private final ValidationService validation;
     private final IdentifierProvider identifier;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public RegistrationFacade(final RegistrationRepository repository, final ValidationService validation, final IdentifierProvider identifier) {
+    public RegistrationFacade(final RegistrationRepository repository, final ValidationService validation, final IdentifierProvider identifier, final BCryptPasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.validation = validation;
         this.identifier = identifier;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
