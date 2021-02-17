f326e90023981a493f7a5be2a32cb0b221e3a7b1
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-17 22:52:01 +0100

    save method logic created. Saving entity to runtime db-HashMap

--- src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
+++ src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
@@ -2,10 +2,17 @@ package com.app.hexagame.inmemorypersistance.registration;
 
 import com.app.hexagame.registration.domain.Registrant;
 import com.app.hexagame.registration.domain.RegistrantRepository;
+import com.app.hexagame.registration.domain.utility.IdProvider;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class InMemoryRegistrantRepository implements RegistrantRepository {
+    private Map<IdProvider, Registrant> runtimeRepo = new HashMap<>();
+
     @Override
     public Registrant save(final Registrant entity) {
-        return null;
+        runtimeRepo = runtimeRepo.put(entity.getId(), entity);
+        return entity;
     }
 }
