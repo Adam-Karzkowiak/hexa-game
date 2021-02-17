b09a8c1de658b27c52a45d2f2221a7cefa29fc72
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-18 00:08:18 +0100

    RegistrantFacadeTest created

---
+++ src/test/java/com/app/hexagame/registration/RegistrantFacadeTest.java
@@ -0,0 +1,12 @@
+package com.app.hexagame.registration;
+
+import org.junit.jupiter.api.Test;
+import org.aspectj.lang.annotation.Before;
+
+class RegistrantFacadeTest {
+
+    
+    void setUp() {
+
+    }
+}
