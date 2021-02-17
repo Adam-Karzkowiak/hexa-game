d69bd2c03b462ff9b4007af8472a83a935ed85bb
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-17 22:31:15 +0100

    save overrided

--- src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
+++ src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
@@ -1,4 +1,11 @@
 package com.app.hexagame.inmemorypersistance.registration;
 
+import com.app.hexagame.registration.domain.Registrant;
+import com.app.hexagame.registration.domain.RegistrantRepository;
+
 public class InMemoryRegistrantRepository implements RegistrantRepository {
+    @Override
+    public Registrant save(final Registrant entity) {
+        return null;
+    }
 }
