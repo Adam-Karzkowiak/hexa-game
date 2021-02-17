48b9dac8672c52a4e2fe7b95032dac73a1ec70e6
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-17 21:30:07 +0100

    createdOn class field removed, unused imports deleted

--- src/main/java/com/app/hexagame/registration/domain/Registrant.java
+++ src/main/java/com/app/hexagame/registration/domain/Registrant.java
@@ -1,26 +1,16 @@
 package com.app.hexagame.registration.domain;
 
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 class Registrant {
-    int id;
-    @Email
-    @NotNull
+    String id;
     String email;
+    Username username;
     Password password;
-    LocalDateTime createdOn;
 
-    Registrant(int id, String email, Password password, LocalDateTime createdOn) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.createdOn = createdOn;
-    }
-
-    int getId() {
+    String getId() {
         return id;
     }
 
