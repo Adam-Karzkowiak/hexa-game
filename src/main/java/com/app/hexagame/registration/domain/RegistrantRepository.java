44c4d3db339055401c23d1050fb7bdf032e57f04
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-23 22:47:14 +0100

    method findById added

--- src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
+++ src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
@@ -1,6 +1,10 @@
 package com.app.hexagame.registration.domain;
 
+import java.util.Optional;
+
 public interface RegistrantRepository {
 
     Registrant save(Registrant entity);
+
+    Optional<Registrant> findById(String id);
 }
