06f7d1c4203f9ab574f4bf01afedfdf23550ccee
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-23 22:56:43 +0100

    method findById changed return value (from Optional<Registrant> to Registrant)

--- src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
+++ src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
@@ -1,10 +1,8 @@
 package com.app.hexagame.registration.domain;
 
-import java.util.Optional;
-
 public interface RegistrantRepository {
 
     Registrant save(Registrant entity);
 
-    Optional<Registrant> findById(String id);
+    Registrant findById(String id);
 }
