777fc38f52d9fb897301177f998fe58450bec328
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-24 23:09:51 +0100

    delete and findAll method added

--- src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
+++ src/main/java/com/app/hexagame/registration/domain/RegistrantRepository.java
@@ -1,8 +1,14 @@
 package com.app.hexagame.registration.domain;
 
+import java.util.List;
+
 public interface RegistrantRepository {
 
     Registrant save(Registrant entity);
 
     Registrant findById(String id);
+
+    List<Registrant> findAll();
+
+    Registrant delete(String id);
 }
