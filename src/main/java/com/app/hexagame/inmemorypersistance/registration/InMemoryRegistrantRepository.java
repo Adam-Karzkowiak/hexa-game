c472f8e514bfcc3763a86ec01f190849a3167522
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-24 23:21:26 +0100

    InMemo repository logic for methods created

--- src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
+++ src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
@@ -3,7 +3,9 @@ package com.app.hexagame.inmemorypersistance.registration;
 import com.app.hexagame.registration.domain.Registrant;
 import com.app.hexagame.registration.domain.RegistrantRepository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class InMemoryRegistrantRepository implements RegistrantRepository {
     private HashMap<String, Registrant> runtimeRepo = new HashMap<>();
@@ -17,5 +19,15 @@ public class InMemoryRegistrantRepository implements RegistrantRepository {
     @Override
     public Registrant findById(String id) {
         return runtimeRepo.get(id);
+    }
+
+    @Override
+    public List<Registrant> findAll() {
+        return new ArrayList<>(runtimeRepo.values());
+    }
+
+    @Override
+    public void delete(final String id) {
+        runtimeRepo.remove(id);
     }
 }
