5df4312857ab4b8f64c29b93494522a4f1d8aa9a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-28 17:45:40 +0100

    Created whole inmemo repository logic (using vavr)

--- src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
+++ src/main/java/com/app/hexagame/inmemorypersistance/registration/InMemoryRegistrantRepository.java
@@ -2,13 +2,17 @@ package com.app.hexagame.inmemorypersistance.registration;
 
 import com.app.hexagame.registration.domain.Registrant;
 import com.app.hexagame.registration.domain.RegistrantRepository;
+import com.app.hexagame.registration.domain.Username;
+import com.app.hexagame.registration.domain.Email;
+import io.vavr.collection.Seq;
+import io.vavr.control.Option;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryRegistrantRepository implements RegistrantRepository {
-    private HashMap<String, Registrant> runtimeRepo = new HashMap<>();
+    private Map<String, Registrant> runtimeRepo = HashMap.empty();
 
     @Override
     public Registrant save(final Registrant entity) {
@@ -17,17 +21,35 @@ public class InMemoryRegistrantRepository implements RegistrantRepository {
     }
 
     @Override
-    public Registrant findById(String id) {
+    public Option<Registrant> findById(String id) {
         return runtimeRepo.get(id);
     }
 
     @Override
     public List<Registrant> findAll() {
-        return new ArrayList<>(runtimeRepo.values());
+        Seq<Registrant> sequence = runtimeRepo.values();
+        return sequence.toJavaList();
     }
 
     @Override
     public void delete(final String id) {
         runtimeRepo.remove(id);
+    }
+
+
+    @Override
+    public boolean existsByEmail(final Email email) {
+        return runtimeRepo.exists(entity -> entity._2
+                .toSimpleModel()
+                .getEmail()
+                .equals(email.getEmail()));
+    }
+
+    @Override
+    public boolean existsByUsername(final Username username) {
+        return runtimeRepo.exists(entity -> entity._2
+                .toSimpleModel()
+                .getUsername()
+                .equals(username.getUsername()));
     }
 }
