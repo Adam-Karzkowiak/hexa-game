7beaceaa7782540cffc18496b83434e73f53013a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-28 17:47:52 +0100

    deleted JpaRepository extension

--- src/main/java/com/app/hexagame/registration/infrastructure/ConfirmationMailSenderAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.app.hexagame.registration.infrastructure;
-
-import com.app.hexagame.registration.domain.ConfirmationMailSender;
-
-class ConfirmationMailSenderAdapter implements ConfirmationMailSender {
-}
--- src/main/java/com/app/hexagame/registration/infrastructure/SqlRegistrantRepository.java
+++ src/main/java/com/app/hexagame/registration/infrastructure/SqlRegistrantRepository.java
@@ -4,5 +4,6 @@ import com.app.hexagame.registration.domain.Registrant;
 import com.app.hexagame.registration.domain.RegistrantRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface SqlRegistrantRepository extends RegistrantRepository, JpaRepository<Registrant, String> {
+interface SqlRegistrantRepository extends RegistrantRepository {
 }
+//TODO JpaRepository<String,Registrant> conflict- getById(String foo)
